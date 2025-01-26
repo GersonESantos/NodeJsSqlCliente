bd5fff12b26c1c1784d0b16d87729040282a04b8
GersonESS <gebhsantos@gmail.com>
2025-01-26 18:18:06 -0300

    a

---
+++ 20 - Imple/app.js
@@ -0,0 +1,98 @@
+// Import the express library
+const express = require('express');
+// importa o módulo fileupload
+const fileUpload = require('express-fileupload');
+// Importa o módulo express-handlebars
+const { engine } = require('express-handlebars');
+
+// importa o módulo mysql
+const mysql = require('mysql2');
+// App
+const app = express();
+// Habilita o fileupload
+app.use(fileUpload());
+
+// Adiciona o bootstrap
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
+
+// configuração do handlebars
+// Adiciona o css
+app.use('/css', express.static('./css'));
+app.use('/imagens', express.static('./imagens'));
+// configuração do handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+// configuração do banco de dados
+// manipulação de dados
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// Conexão com o banco de dados
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Gabibi89*',
+    database: 'projeto'
+}); 
+// Conectar
+conexao.connect(function(err){
+    if(err) throw err;
+    console.log('Conectado com sucesso!');
+});
+// Rota Principal
+app.get('/', (req, res) => {
+    let sql = 'SELECT * FROM cliente';  
+    conexao.query(sql, function(err, result){
+        if(err) throw err;
+        res.render('formulario', {clientes: result});
+    });
+});
+
+//Rota de cadastro
+
+// app.post('/cadastrar', (req, res) => {
+//     req.files.imagem.mv(__dirname+'/imagens/'+req.files.imagem.name);
+//     const { nome, idade, email, senha, imagem} = req.body;
+//     const sql = `INSERT INTO cliente (nome, idade, email, senha, imagem) VALUES ('${nome}', ${idade}, '${email}', '${senha}', '${(req.files.imagem.name)}')`;
+//     conexao.query(sql, function(err, result){
+//         if(err) throw err;
+//         console.log('Usuário cadastrado com sucesso!');
+//         res.render('formulario');
+//     });
+// });
+
+app.post('/cadastrar', function(req, res){
+    let nome = req.body.nome;
+    let idade = req.body.idade;
+    let email = req.body.email;
+    let senha = req.body.senha;
+    let imagem = req.files.imagem;
+    //Sql
+    let sql = `INSERT INTO cliente (nome, idade, email, senha, imagem) VALUES ('${nome}', ${idade}, '${email}', '${senha}', '${imagem.name}')`;
+    //executar a query SQL
+    conexao.query(sql, function(err, result){
+        if(err) throw err;
+        console.log('Usuário cadastrado com sucesso!');
+        req.files.imagem.mv(__dirname+'/imagens/'+req.files.imagem.name);
+        console.log('resultado', result);
+    });
+    res.render('/');
+});
+
+// Rota de exclusão de cliente
+app.get('/remover/:id&:imagem', function(req, res) {
+    console.log(req.params.id);
+    console.log(req.params.imagem);
+    res.end();
+    // let id = req.params.id;
+    // let sql = `DELETE FROM cliente WHERE id = ${id}`;
+    // conexao.query(sql, function(err, result){
+    //     if(err) throw err;
+    //     console.log('Cliente excluído com sucesso!');
+    //     res.redirect('/');
+    // });
+});
+app.listen(8080, () => {
+    console.log('Rodando app listening at http://localhost:8080');
+  });
