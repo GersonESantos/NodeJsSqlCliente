3c621f1ea45bb3ed8cbc97b30c784b5653a8802c
GersonESS <gebhsantos@gmail.com>
2025-01-29 14:55:34 -0300

    Update app.js

--- 21 - Rota/app.js
+++ 21 - Rota/app.js
@@ -94,6 +94,13 @@ app.get('/remover/:id&:imagem', function(req, res){
 
 });
 
+// Rota para redirecionar para o formulário de alteração/edição
+app.get('/formularioEditar/:id', function(req, res){
+    
+    console.log(req.params.id);
+    res.end();
+    
+});
 
 app.listen(8080, () => {
     console.log('Rodando app listening at http://localhost:8080');
