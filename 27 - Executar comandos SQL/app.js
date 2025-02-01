6b5147816a948c00de9e0214fe0b53781b9cf4be
GersonESS <gebhsantos@gmail.com>
2025-02-01 20:26:14 -0300

    a

--- 27 - Executar comandos SQL/app.js
+++ 27 - Executar comandos SQL/app.js
@@ -7,9 +7,8 @@ const { engine } = require('express-handlebars');
 
 // importa o módulo mysql
 const mysql = require('mysql2');
-//file systems
+// File System
 const fs = require('fs');
-
 // App
 const app = express();
 // Habilita o fileupload
@@ -47,7 +46,7 @@ app.get('/', (req, res) => {
     let sql = 'SELECT * FROM cliente';
     conexao.query(sql, function(err, result){
         if(err) throw err;
-        res.render('formulario', {cliente: result});
+        res.render('formulario', {clientes: result});
     });
 });
 
@@ -72,119 +71,70 @@ app.post('/cadastrar', function(req, res){
    res.redirect('/');
     //res.end();
 });
+
+
+
 // Rota para remover produtos
 app.get('/remover/:id&:imagem', function(req, res){
     
-    // SQL
-    let sql = `DELETE FROM cliente WHERE id = ${req.params.id}`;
+    // Tratamento de exeção
+    try{
+        // SQL
+        let sql = `DELETE FROM produtos WHERE codigo = ${req.params.id}`;
 
-    // Executar o comando SQL
-    conexao.query(sql, function(erro, retorno){
-        // Caso falhe o comando SQL
-        if(erro) throw erro;
-
-        // Caso o comando SQL funcione
-        fs.unlink(__dirname+'/imagens/'+req.params.imagem, (erro_imagem)=>{
-            console.log('Falha ao remover a imagem ');
+        // Executar o comando SQL
+        conexao.query(sql, function(erro, retorno){
+            // Remover imagem
+            fs.unlink(__dirname + '/imagens/' + req.params.imagem, (erro_imagem) => {
+                if (erro_imagem) {
+                  console.log("Falha ao remover a imagem: ", erro_imagem);
+                } else {
+                  console.log("Imagem removida com sucesso.");
+                }
+            });
         });
-    });
 
-    // Redirecionamento
-    res.redirect('/');
-
+        // Redirecionamento
+        //res.redirect('/okRemover');
+        res.redirect('/');
+    }catch(erro){
+        //res.redirect('/falhaRemover');
+        res.redirect('/');
+    }
 });
 
 // Rota para redirecionar para o formulário de alteração/edição
 app.get('/formularioEditar/:id', function(req, res){
+    
     let sql = `SELECT * FROM cliente WHERE id = ${req.params.id}`;
     conexao.query(sql, function(err, retorno){
         if(err) throw err;
         res.render('formularioEditar', {cliente:retorno[0]});
     });    
 });
-
 // Rota para editar Cliente
-// app.post('/editar', function(req, res){
-//     //obter os dados do formulário
-//    let id = req.body.id;
-//     let nome = req.body.nome;
-//     let telefone = req.body.telefone;
-//     let email = req.body.email;
-//     let afinidade = req.body.afinidade;
-//     let nomeImagem = req.body.nomeImagem;
-//     // verificar se o campo imagem foi preenchido
-//     try{
-//         let imagem = req.files.imagem;
-        
-
-//     }catch(erro){
-    //     }
-    //     res.redirect('/');
+app.post('/editar', function(req, res){
+    //obter os dados do formulário
+    let id = req.body.id;
+    let nome = req.body.nome;
+    let telefone = req.body.telefone;
+    let email = req.body.email;
+    let afinidade = req.body.afinidade;
+    let nomeImagem = req.body.nomeImagem;
     
-    //     });
+
+    // definir o tipo de ediçâo
+    try{
+        let imagem = req.files.imagem;
+        //sql
+        let sql = `UPDATE cliente SET nome = '${nome}', telefone = ${telefone}, email = '${email}', afinidade = '${afinidade}', imagem = '${imagem.name}' WHERE id = ${id}`;
+    }catch(erro){
+        let sql = `UPDATE cliente SET nome = '${nome}', telefone = ${telefone}, email = '${email}', afinidade = '${afinidade}' WHERE id = ${id}`;
+    }
+        res.end;
+    });
     // verificar se o campo imagem foi preenchido
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // Rota para editar produtos
-    app.post('/editar', function(req, res){
-           
-        let id = req.body.id;
-        let nome = req.body.nome;
-        let telefone = req.body.telefone;
-        let email = req.body.email;
-        let afinidade = req.body.afinidade;
-        let nomeImagem = req.body.nomeImagem;
-        // verificar se o campo imagem foi preenchido       // Definir o tipo de edição
-        try{
-            // Objeto de imagem
-            let imagem = req.files.imagem.name;
-            
-            // SQL
-            let sql = `UPDATE cliente SET nome='${nome}', telefone=${telefone}, email='${email}', afinidade='${afinidade}', imagem='${imagem.name}' WHERE id=${id}`;    
-            l
-            // Executar comando SQL
-            conexao.query(sql, function(erro, retorno){
-                // Caso falhe o comando SQL
-                if(erro) throw erro;
-                
-                // Remover imagem antiga
-                fs.unlink(__dirname+'/imagens/'+nomeImagem, (erro_imagem)=>{
-                    console.log('Falha ao remover a imagem.');
-                });
-                
-                // Cadastrar nova imagem
-                imagem.mv(__dirname+'/imagens/'+imagem.name);
-            });
-        }catch(erro){
-            
-            // SQL
-        
-             let sql = `UPDATE cliente SET nome='${nome}', telefone=${telefone}, email='${email}', afinidade='${afinidade}' WHERE id=${id}`;    
-    
-        // Executar comando SQL
-        conexao.query(sql, function(erro, retorno){
-            // Caso falhe o comando SQL
-            if(erro) throw erro;
-        });
-    }
-
-    // Finalizar rota
-    res.redirect(`/`)
-    
-});
-
 app.listen(8080, () => {
     console.log('Rodando app listening at http://localhost:8080');
   });
-    
