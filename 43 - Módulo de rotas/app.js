6804e5558f2857a52eb38e7d42e16283baf5c52b
GersonESS <gebhsantos@gmail.com>
2025-02-05 20:46:39 -0300

    a

--- 43 - Módulo de rotas/app.js
+++ 43 - Módulo de rotas/app.js
@@ -6,9 +6,9 @@ const fileUpload = require('express-fileupload');
 // Importa o módulo express-handlebars
 const { engine } = require('express-handlebars');
 
+// Importar o módulo de rotas
+const rota_cliente = require('./rotas/cliente_rotas');
 
-// Importar módulo de servico
-const servico = require('./servicos/cliente_servico');
 // App
 const app = express();
 // Habilita o fileupload
@@ -36,55 +36,10 @@ app.set('views', './views');
 // manipulação de dados
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+// Rotas
+app.use('/', rota_cliente);
 
 
-// Rota Principal
-app.get('/', (req, res) => {
-    servico.formularioCadastro(req, res);
-    
-});
-// Rota Principal contendo a situação do cadastro
-app.get('/:situacao', (req, res) => {
-    
-    servico.formularioCadastroComSituacao(req, res);
-   
-});
-
-// Rota de cadastro
-app.post('/cadastrar', function(req, res){
-   servico.cadastrarCliente(req, res); 
- });
-
-
-// Rota para remover clientes
-app.get('/remover/:id&:imagem', function(req, res){
-    servico.removerCliente(req, res);
-   
-});
-
-// Rota para redirecionar para o formulário de alteração/edição
-app.get('/formularioEditar/:id', function(req, res){
-    servico.formularioEditar(req, res);
-    
-});
-// Rota para editar Cliente
-app.post('/editar', function(req, res){
-    //obterse os dados do formulário
-    servico.editarCliente(req, res);
-    
-    });
-
-    // Rota para listar clientes   '${afinidade}'
-app.get('/listar/:afinidade', function(req, res){
-    servico.listagemCliente(req, res);
-    
-             
-});
-// Rota para Pesquisa
-
-app.post('/pesquisa', function(req, res){
-    servico.pesquisa(req, res);
-});
 app.listen(8080, () => {
     console.log('Rodando app listening at http://localhost:8080');
   });
